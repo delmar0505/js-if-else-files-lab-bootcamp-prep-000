4f4518b5a0f908c4a381af0956aca58aa3ec1d0e
oscar leon <first.liberal.thinker@gmail.com>
2018-08-09 15:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var name = "susan"
+var name = "joe"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
-if (name === "susan"){console.log("the name is correct")}
-else{console.log ("expected the name to be susan")}
